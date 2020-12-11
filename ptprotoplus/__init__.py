8c3172b4b833ce3fc6a973d89f4ebb5fd0a7deb3
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-11 18:09:49 +0000

    First commit

---
+++ ptprotoplus/__init__.py
@@ -0,0 +1,9 @@
+from pitop.protoplus import (  # noqa: F401
+    adc,
+    audio,
+    psonic,
+    sensors
+)
+print("Note: Use of the 'ptprotoplus' package is now deprecated. Please use 'pitop.protoplus' instead.")
+# TODO
+print("For more information, please see <TODO: URL>")
