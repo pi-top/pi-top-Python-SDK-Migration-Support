c10b4b4b61007cbc0a182a73f1447cbd48c2441f
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-12-16 16:34:23 +0000

    Update protoplus

--- ptprotoplus/__init__.py
+++ ptprotoplus/__init__.py
@@ -1,9 +1,8 @@
 from pitop.protoplus import (  # noqa: F401
     adc,
-    audio,
-    psonic,
     sensors
 )
+import psonic  # noqa: F401
 print("Note: Use of the 'ptprotoplus' package is now deprecated. Please use 'pitop.protoplus' instead.")
-# TODO
+print("Use of 'ptprotoplus.psonic' package is also deprecated. Please use 'psonic' directly instead.")
 print("For more information, please see <TODO: URL>")
