02e6fa72d4b899066f753e995bf25e074ed32c7e
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-07 16:38:44 +0000

    Move deprecated OLED functions to import

--- lib/ptoled/__init__.py
+++ lib/ptoled/__init__.py
@@ -4,15 +4,32 @@ from pitop.miniscreen.oled import (  # noqa: F401
 from pitop.miniscreen.oled.core import (  # noqa: F401
     fps_regulator,
 )
-from pitop.miniscreen.oled.core.controls import (  # noqa: F401
-    get_device as get_device_instance,
-    device_is_active as device_reserved,
-    reset_device as reset_device_instance,
-    set_control_to_pi as set_oled_control_to_pi,
-    set_control_to_hub as set_oled_control_to_hub
-)
-import pitop.miniscreen.oled.core.controls as device_helper  # noqa: F401
-canvas = PTOLEDDisplay().canvas
+
+
+__o = PTOLEDDisplay()
+canvas = __o.canvas
+
+
+# Deprecated functions
+def get_device_instance():
+    return __o.get_device()
+
+
+def device_reserved():
+    return __o.device_is_active()
+
+
+def reset_device_instance():
+    __o.reset_device()
+
+
+def set_oled_control_to_pi():
+    __o.set_control_to_pi()
+
+
+def set_oled_control_to_hub():
+    __o.set_control_to_hub()
+
 
 print("Note: Use of the 'ptoled' package is now deprecated. Please use 'pitop.miniscreen.oled' instead.")
 print("For more information, please see https://github.com/pi-top/pi-top-Python-SDK-Migration-Support")
