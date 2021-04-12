59b1e487cfe3cf743d892fe59944d6ddc141e28c
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-04-12 12:55:50 +0100

    Move pma.components out of SDK

---
+++ lib/pitop/pma/components.py
@@ -0,0 +1,7 @@
+from .__init__ import *  # noqa: F401, F403
+
+# This file remains to support legacy user importing
+# from here: e.g. "from pitop.pma.components import Button"
+
+print("Note, Use of 'pitop.pma.components' is now deprecated. Please use 'pitop.pma' instead.")
+print("For more information, please see https://github.com/pi-top/pi-top-Python-SDK-Migration-Support")
